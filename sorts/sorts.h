4d25081b647db40c27596f203ae7f5496189fb6b
|GriB28| <gp.off.dev@gmail.com>
2026-02-08 20:56:20 +0300

    added snapshot saving to sort algorithms

--- sorts/sorts.h
+++ sorts/sorts.h
@@ -2,24 +2,39 @@
 #define SORTS_H
 
 #include <cstddef>
+#include <fstream>
 using std::size_t;
+using std::ofstream;
 
 
-namespace sorts {
-    void bubble(int* list, const size_t &length);
-    void insertion(int* list, const size_t &length);
-    void selection(int* list, const size_t &length);
+class Sorts {
+public:
+    explicit Sorts(ofstream* link);
+    ~Sorts() = default;
 
+    void load_list(int* array, const size_t &array_length);
 
-    void merge(int* list, const size_t &length);
-    void merge(int* list, const size_t &left, const size_t &right);
-    void do_merge(int* list, const size_t &left, const size_t &middle, const size_t &right);
+    void bubble() const;
+    void insertion() const;
+    void selection() const;
 
-    void quick(int* list, const size_t &length);
-    void quick(int* list, const size_t &low, const size_t &high);
+    void merge();
+    void merge(const size_t &left, const size_t &right);
 
-    void heap(int* list, const size_t &length);
-    void do_heap(int* list, const size_t &length, const size_t &index);
-}
+    void quick();
+    void quick(const size_t &low, const size_t &high);
+
+    void heap();
+private:
+    int* list;
+    size_t length;
+
+    void do_merge(const size_t &left, const size_t &middle, const size_t &right) const;
+    void do_heap(const size_t &len, const size_t &index);
+
+    void save_snapshot() const;
+
+    ofstream* link;
+};
 
 #endif
