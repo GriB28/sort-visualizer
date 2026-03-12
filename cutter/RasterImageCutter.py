b48efe09d6cc83397c1db5607755ab6bfe700b82
Arseny Kenunen <arsenykenunen@gmail.com>
2026-03-12 14:16:23 +0300

    video attempt

--- cutter/RasterImageCutter.py
+++ cutter/RasterImageCutter.py
@@ -37,4 +37,54 @@ cv2.imwrite('slicedrick.png', reconstructed)
 fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 video = cv2.VideoWriter(outputname, fourcc, framerate, (width, height))
 
+def check(i, j, stripe):
+    if stripe[i, :] == stripe[j, :]:
+        return 0
+    else:
+        if stripe[i, :] < stripe[j, :]:
+            return 1
+        else:
+            return -1
+def swap(i, j, stripe):
+    if check(i, j, stripe) == 1:
+        tmp = stripe[i, :]
+        stripe[i, :] = stripe[j, :]
+        stripe[j, :] = tmp
+def sort_cycles(stripe):
+    n = height
+    swapped = False
+    for i in range(n - 1):
+        if stripe[i, 1] == 255 and stripe[i+1, 1] == 0:
+            stripe[i, :], stripe[i+1, :] = stripe[i+1, 1], stripe[i, 1]
+            swapped = True
+    #print(swapped)
+    return swapped
+
+
+sorted_flags = [False] * array_size     # track which arrays are already sorted
+iteration = 0
+while not all(sorted_flags):
+    iteration += 1
+    print(f"iteration {iteration}")
+    any_swaps_this_iter = False
+
+    for i, stripe in enumerate(vertical_stripes):
+        #print(iteration, i)
+        if not sorted_flags[i]:
+            swapped = sort_cycles(stripe)
+            if not swapped:
+                sorted_flags[i] = True  # this array is now sorted
+            else:
+                any_swaps_this_iter = True
+    #print(sorted_flags)
+    # If no array made a swap in this iteration, all are sorted – stop early
+    if not any_swaps_this_iter:
+        break
+    if iteration % 10 == 0:
+        frame = np.hstack(vertical_stripes)
+        video.write(cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR))
+        print("write")
+
+video.release()
+
 
